8a771ad73c26a241ff692fefbd5c5572d72d5b21
Leonardo Vales <leonardo.vales@getrak.com.br>
2022-12-05 09:46:38 -0300

    Implementando configuração swc

--- src/infrastructure/repository/product.repository.spec..ts
+++ src/infrastructure/repository/product.repository.spec..ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript"
+import ProductModel from "../db/sequelize/model/product.model"
 
 describe('ProductRepository tests', () => {
   let sequelize: Sequelize
@@ -10,7 +11,12 @@ describe('ProductRepository tests', () => {
       logging: false,
       sync: { force: true },
     })
+
+  sequelize.addModels([ProductModel]);
+  await sequelize.sync()
   })
+
+  
 
   afterEach( async () => {
     await sequelize.close()
